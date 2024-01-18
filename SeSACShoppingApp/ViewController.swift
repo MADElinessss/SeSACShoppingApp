57a77c35ad2d13637ed9c2fb1ac99809d0a06ece
MADElinessss <jyseen@naver.com>
2024-01-18 16:13:30 +0900

    Initial Commit

---
+++ SeSACShoppingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACShoppingApp
+//
+//  Created by Madeline on 2024/01/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
